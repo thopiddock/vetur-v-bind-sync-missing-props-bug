604b5900f5cdf66d1ad1dbf1f07952508301cb59
Tom Piddock <tpiddock@gmail.com>
2021-09-06 22:47:22 +0100

    Add 4 types of binding to child properties. `v-bind.sync` produces false negative.

--- src/Test.vue
+++ src/Test.vue
@@ -1,13 +1,39 @@
 <template>
   <div>
-    {{ fo }}
+    <!-- Standard property - sync -->
+    <child :title="title" :content="content" />
+    
+    <!-- Standard property + sync -->
+    <child :title.sync="title" :content.sync="content" />
+
+    <!-- v-bind wrapped - sync -->
+    <child v-bind="wrapped" />
+
+    <!-- v-bind wrapped + sync -->
+    <child v-bind.sync="wrapped" />
   </div>
 </template>
 
 <script>
 import * as _ from 'lodash'
+import Child from './Child.vue'
 
 export default {
-  props: ['foo', 'bar']
+  components: { Child },
+  props: ['foo', 'bar'],
+  data() {
+    const title = 'Title of win'
+    const content = "I'm some content"
+
+    return {
+      title,
+      content,
+
+      wrapped: {
+        title,
+        content
+      }
+    }
+  }
 }
 </script>
