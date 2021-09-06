59e67f79cc38308685f7ad5f64958ad8b6f81051
Tom Piddock <tpiddock@gmail.com>
2021-09-06 22:46:58 +0100

    Add Child component with optional and required properties.

---
+++ src/Child.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <h1>{{ title }}</h1>
+    <p>{{ content }}</p>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    title: { type: String, required: true },
+    content: { type: String, required: false }
+  }
+}
+</script>
